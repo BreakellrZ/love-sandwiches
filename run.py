38817daea30fd9965fcc557dd545cb0a89b4372a
Colin Breakell <cbreakell99@gmail.com>
2024-02-23 17:24:55 +0000

    Added validarte_data function so that if user does not put in exactly 6 values, they will get an error printed to them

--- run.py
+++ run.py
@@ -16,11 +16,30 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter salesdata from the last market.")
+    print("Please enter sales data from the last market.")
     print("Data should be six numbers, seperated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your dada here")
-    print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values to integers.
+    Raises ValueError if strings cannot be converted to int,
+    or if there arent exactly 6 values.
+    """
+
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again. \n")
+
+    
 
 get_sales_data()
